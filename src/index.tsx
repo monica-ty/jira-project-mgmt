952e76b967738e0d4887b601d01b8d15f08ce631
monica-ty <acamonica@163.com>
2022-03-25 16:50:56 -0400

    feat: add login

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+);
 ReactDOM.render(
   <React.StrictMode>
     <App />
